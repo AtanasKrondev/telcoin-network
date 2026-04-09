add77f3a3de30afeaf1cb91146f8e01ae1671c17
Atanas Krondev <krondev@gmail.com>
2026-04-09 13:01:56 +0300

    feat: docker compose 6 validators

--- etc/genesis.sh
+++ etc/genesis.sh
@@ -6,18 +6,20 @@ cp -r /home/nonroot/data/validator-1/node-info.yaml /home/nonroot/data/genesis/v
 cp -r /home/nonroot/data/validator-2/node-info.yaml /home/nonroot/data/genesis/validators/validator-2.yaml
 cp -r /home/nonroot/data/validator-3/node-info.yaml /home/nonroot/data/genesis/validators/validator-3.yaml
 cp -r /home/nonroot/data/validator-4/node-info.yaml /home/nonroot/data/genesis/validators/validator-4.yaml
+cp -r /home/nonroot/data/validator-5/node-info.yaml /home/nonroot/data/genesis/validators/validator-5.yaml
+cp -r /home/nonroot/data/validator-6/node-info.yaml /home/nonroot/data/genesis/validators/validator-6.yaml
 
 /usr/local/bin/telcoin genesis \
     --datadir /home/nonroot/data/ \
     --chain-id 0x1e7 \
     --epoch-duration-in-secs 60 \
-    --dev-funded-account 0x3DCc9a6f3A71F0A6C8C659c65558321c374E917a \
+    --dev-funded-account 0x748Cab9A6993A24CA6208160130b3f7b79098c6d \
     --max-header-delay-ms 1000 \
     --min-header-delay-ms 1000 \
-    --consensus-registry-owner 0x3DCc9a6f3A71F0A6C8C659c65558321c374E917a
+    --consensus-registry-owner 0x748Cab9A6993A24CA6208160130b3f7b79098c6d
 
 # create directories and copy files for each validator
-for i in {1..4}; do
+for i in {1..6}; do
     mkdir -p /home/nonroot/data/validator-$i/genesis/
     cp /home/nonroot/data/genesis/genesis.yaml /home/nonroot/data/genesis/committee.yaml /home/nonroot/data/validator-$i/genesis/
     cp /home/nonroot/data/parameters.yaml /home/nonroot/data/validator-$i/
